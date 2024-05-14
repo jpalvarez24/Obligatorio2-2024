1ae86226a8b00946b0a69a4a95e8cf660c527586
Juan Pedro <alvarezcorespapetu@gmail.com>
2024-05-14 09:31:19 -0300

    Agrego el proyecto

---
+++ Obligatorio2-2024/src/obligatorio2/pkg2024/Obligatorio22024.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package obligatorio2.pkg2024;
+
+/**
+ *
+ * @author Juan Pedro Alvarez-281369
+ */
+public class Obligatorio22024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
